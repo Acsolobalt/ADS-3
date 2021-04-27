041d656fa66a66272d066e41403a737e5e6b8812
Acsolobalt <78843052+Acsolobalt@users.noreply.github.com>
2021-04-27 19:59:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,104 @@
 #include <string>
 #include "tstack.h"
 
+int countPriory(char a) {
+if (a == '+' || a == '-') {
+return 2;
+}
+if (a == '*' || a == '/') {
+return 3;
+}
+if (a == '(') {
+return 0;
+}
+if (a == ')') {
+return 1;
+}
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
-  return std::string("");
+TStack<char> mstack;
+string result = "";
+for (int i = 0; i < inf.size(); ++i) {
+if (inf[i] > '9' || inf[i] < '0' || (inf[i] != '(' && inf[i] != ')' && inf[i] != '+' && inf[i] != '-' && inf[i] != '*' && inf[i] != '/')) {
+if (inf[i] <= '9' && inf[i] >= '0') {
+result = result + inf[i] + ' ';
+} else {
+if (countPriory(inf[i]) == 1) {
+while (mstack.get() != '(') {
+result = result + mstack.get() + ' ';
+mstack.pop();
+}
+mstack.pop();
+}
+else {
+if (mstack.isEmpty() || countPriory(inf[i]) == 0 || countPriory(inf[i]) > countPriory(mstack.get())) {
+mstack.push(inf[i]);
+} else {
+result = result + mstack.get() + ' ';
+mstack.pop();
+mstack.push(inf[i]);
+}
+}
+}
+}
+}
+while (!mstack.isEmpty()) {
+result = result + mstack.get() + ' ';
+mstack.pop();
+}
+result.erase(result.end() - 1);
+return result;
+return std::string("");
 }
 
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+TStack<char> mstack;
+int a, b, sum;
+char result;
+for (int i = 0; i < pst.size(); ++i) {
+if (pst[i] == ' ') {
+continue;
+}
+if (pst[i] <= '9' && pst[i] >= '0') {
+mstack.push(pst[i]);
+}
+else {
+switch (pst[i]) {
+case '+':
+a = mstack.get() - '0';
+mstack.pop();
+b = mstack.get() - '0';
+mstack.pop();
+sum = b + a;
+result = sum + '0';
+mstack.push(result); break;
+case '-':
+a = mstack.get() - '0';
+mstack.pop();
+b = mstack.get() - '0';
+mstack.pop();
+sum = b - a;
+result = sum + '0';
+mstack.push(result); break;
+case '*':
+a = mstack.get() - '0';
+mstack.pop();
+b = mstack.get() - '0';
+mstack.pop();
+sum = b * a;
+result = sum + '0';
+mstack.push(result); break;
+case '/':
+a = mstack.get() - '0';
+mstack.pop();
+b = mstack.get() - '0';
+mstack.pop();
+sum = b / a;
+result = sum + '0';
+mstack.push(result); break;
+}
+}
+}
+return mstack.get() - '0';
 }
